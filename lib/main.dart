df284ceea6df76e70fc4644fdff56e8b5944a1b1
ricegummy <himrza@gmail.com>
2022-07-09 18:23:49 +0900

    login + sign up UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:mental_health_app/pages/dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mental_health_app/startup/login/login.dart';
+import 'package:mental_health_app/startup/sign_up/sign_up.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -12,7 +20,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Dashboard(),
+      //home: Dashboard(),
+
+      initialRoute: '/',
+      routes: {
+        //'/' :(context) => StartUpPage(),
+        '/' :(context) => LoginPage(),
+        '/register' :(context) => SignUpPage(),
+        '/dashboard' :(context) =>Dashboard(),
+      },
+
     );
   }
 }
